a0b5cd2bf9b0fbdc2fa36712ffb670a9970e9a86
Suxin Li <lisuxin4@gmail.com>
2019-11-07 21:00:24 +0800

    version 1

---
+++ print.cpp
@@ -0,0 +1,6 @@
+#include "print.h"
+void print(int num) {
+	if (num >= 100) {
+		cout << num << " is a large number." << endl;
+	}
+}
---
+++ test_print.cpp
@@ -0,0 +1,6 @@
+#include "print.h"
+
+int main() {
+	print(100);
+	return 0;
+}
