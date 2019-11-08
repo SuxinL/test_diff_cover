b0b0f3d14ecef167b1b2d4a0fec28a04a9967e5b
Suxin Li <lisuxin4@gmail.com>
2019-11-08 16:57:41 +0800

    change 1

--- print.cpp
+++ print.cpp
@@ -3,4 +3,9 @@ void print(int num) {
 	if (num >= 100) {
 		cout << num << " is a large number." << endl;
 	}
+	if (num & 1) {
+		cout << num << " is an odd number." << endl;
+	} else {
+		cout << num << " is an odd number." << endl;
+	}
 }
--- test_print.cpp
+++ test_print.cpp
@@ -2,5 +2,9 @@
 
 int main() {
 	print(100);
+	int i = 1;
+	if (i > 2) {
+		cout << "hello world" << endl;
+	}
 	return 0;
 }
